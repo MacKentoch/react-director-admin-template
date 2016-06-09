bebf1f1ec515433c7c1b3124d6ac68a87f21fec6
MacKentoch <erwan.datin@gmail.com>
2016-06-09 11:55:41 +0200

    chore : add dependencies

--- src/app/index.js
+++ src/app/index.js
@@ -9,8 +9,24 @@ import 'babel-polyfill';
 import 'animate.css';
 import 'jquery';
 import 'font-awesome/css/font-awesome.min.css';
+import 'ionicons/dist/css/ionicons.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
+//
+import './vendors/css/morris/morris.css';
+import './vendors/css/jvectormap/jquery-jvectormap-1.2.2.css';
+import './vendors/css/datepicker/datepicker3.css';
+import './vendors/css/daterangepicker/daterangepicker-bs3.css';
+import './vendors/css/iCheck/all.css';
+import './vendors/css/director-style.css';
+
+import './vendors/js/jquery-ui-1.10.3.min.js';
+import './vendors/js/plugins/daterangepicker/daterangepicker.js';
+import './vendors/js/plugins/chart.js';
+import './vendors/js/plugins/iCheck/icheck.min.js';
+import './vendors/js/plugins/fullcalendar/fullcalendar.js';
+
+
 import './style/index.style.scss';
 
 const ELEMENT_TO_BOOTSTRAP  = 'root';
