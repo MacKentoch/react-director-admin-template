c21288be93b58ae4659aa372d22ae2d92119c29c
MacKentoch <erwan.datin@gmail.com>
2016-06-14 17:11:41 +0200

    chore : UserPanel

--- src/app/components/aside/asideLeft/userPanel/UserPanel.js
+++ src/app/components/aside/asideLeft/userPanel/UserPanel.js
@@ -5,19 +5,22 @@ import React, { PropTypes } from 'react';
 const UserPanel = (props) => {
   return (
     <div className="user-panel">
-      <div className="pull-left image">
-        <img
-          src={require('../../../../vendors/img/26115.jpg')}
-          className="img-circle"
-          alt="User Image"
-        />
-      </div>
+      {
+        props.showUserPicture &&
+        <div className="pull-left image">
+          <img
+            src={require('../../../../vendors/img/26115.jpg')}
+            className="img-circle"
+            alt="User Image"
+          />
+        </div>
+      }
       <div className="pull-left info">
         <p>
           { `${props.hello} ${props.username}` }
         </p>
-        <a href="#">
-
+        <a
+          href="#">
           { props.online &&
             <i className="fa fa-circle text-success"></i>
           }
@@ -41,6 +44,8 @@ const UserPanel = (props) => {
 UserPanel.propTypes = {
   hello: PropTypes.string,
   username: PropTypes.string,
+  showUserPicture: PropTypes.bool,
+  userPicture: PropTypes.any,
   connectionStatus: PropTypes.shape({
     online: PropTypes.string,
     disconnected: PropTypes.string
@@ -55,7 +60,8 @@ UserPanel.defaultProps = {
     online: 'Online',
     disconnected: 'Disconnected'
   },
-  online: false
+  online: false,
+  showUserPicture: true
 };
 
 export default UserPanel;
