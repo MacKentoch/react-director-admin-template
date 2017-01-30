0a708d63b340af044373801cb153df3ba2fe79e5
MacKentoch <erwan.datin@gmail.com>
2017-01-30 05:15:11 +0100

    BasicProgressBar

--- src/app/views/basicProgressBar/BasicProgressBar.js
+++ src/app/views/basicProgressBar/BasicProgressBar.js
@@ -2,8 +2,8 @@ import React, {
   PropTypes,
   Component
 }                         from 'react';
-import cx                 from 'classnames';
 import {
+  AnimatedView,
   Panel,
   ProgressBar as ProgressBarComponent
 }                         from '../../components';
@@ -12,22 +12,9 @@ import Highlight          from 'react-highlight';
 
 
 class BasicProgressBar extends Component {
-
-  state = {
-    animated: true,
-    viewEnters: false
-  };
-
   componentWillMount() {
     const { actions: { enterBasicProgressBar } } = this.props;
     enterBasicProgressBar();
-  }
-
-  componentDidMount() {
-    this.enterAnimationTimer = setTimeout(
-      () => this.setState({viewEnters: true}),
-      500
-    );
   }
 
   shouldComponentUpdate(nextProps, nextState) {
@@ -37,12 +24,9 @@ class BasicProgressBar extends Component {
   componentWillUnmount() {
     const { actions: { leaveBasicProgressBar } } = this.props;
     leaveBasicProgressBar();
-    clearTimeout(this.enterAnimationTimer);
   }
 
   render() {
-    const { animated, viewEnters } = this.state;
-
     const source = `
       // import
       import { ProgressBar } from './_SOMEWHERE_/components';
@@ -122,99 +106,92 @@ class BasicProgressBar extends Component {
       `;
 
     return(
-      <section className={
-        cx({
-          'content':       true,
-          'animatedViews': animated,
-          'invisible':     !viewEnters,
-          'view-enter':    viewEnters
-        })
-      }>
-      {/* preview: */}
-      <div className="row">
-        <div className="col-xs-12">
-          <Panel
-            title="Basic Progress Bars"
-            hasTitle={true}>
-            <p>
-              <code>
-                .progress
-              </code>
-            </p>
-            <ProgressBarComponent
-              barSize="progress"
-              barStriped={false}
-              barStyle="primary"
-              active={false}
-              valueNow={40}
-              valueMin={0}
-              valueMax={100}
-              screenReadersText={`${40}% Complete (success)`}
-            />
-            <p>
-              Class:
-              <code>
-                .sm
-              </code>
-            </p>
-            <ProgressBarComponent
-              barSize="sm"
-              barStriped={false}
-              barStyle="success"
-              active={true}
-              valueNow={20}
-              valueMin={0}
-              valueMax={100}
-              screenReadersText={`${20}% Complete`}
-            />
-            <p>
-              Class:
-              <code>
-                .xs
-              </code>
-            </p>
-            <ProgressBarComponent
-              barSize="xs"
-              barStriped={false}
-              barStyle="warning"
-              active={false}
-              valueNow={60}
-              valueMin={0}
-              valueMax={100}
-              screenReadersText={`${60}% Complete (warning)`}
-            />
-            <p>
-              Class:
-              <code>
-                .xxs
-              </code>
-            </p>
-            <ProgressBarComponent
-              barSize="xxs"
-              barStriped={false}
-              barStyle="danger"
-              active={false}
-              valueNow={60}
-              valueMin={0}
-              valueMax={100}
-              screenReadersText={`${60}% Complete (warning)`}
-            />
-          </Panel>
+      <AnimatedView>
+        {/* preview: */}
+        <div className="row">
+          <div className="col-xs-12">
+            <Panel
+              title="Basic Progress Bars"
+              hasTitle={true}>
+              <p>
+                <code>
+                  .progress
+                </code>
+              </p>
+              <ProgressBarComponent
+                barSize="progress"
+                barStriped={false}
+                barStyle="primary"
+                active={false}
+                valueNow={40}
+                valueMin={0}
+                valueMax={100}
+                screenReadersText={`${40}% Complete (success)`}
+              />
+              <p>
+                Class:
+                <code>
+                  .sm
+                </code>
+              </p>
+              <ProgressBarComponent
+                barSize="sm"
+                barStriped={false}
+                barStyle="success"
+                active={true}
+                valueNow={20}
+                valueMin={0}
+                valueMax={100}
+                screenReadersText={`${20}% Complete`}
+              />
+              <p>
+                Class:
+                <code>
+                  .xs
+                </code>
+              </p>
+              <ProgressBarComponent
+                barSize="xs"
+                barStriped={false}
+                barStyle="warning"
+                active={false}
+                valueNow={60}
+                valueMin={0}
+                valueMax={100}
+                screenReadersText={`${60}% Complete (warning)`}
+              />
+              <p>
+                Class:
+                <code>
+                  .xxs
+                </code>
+              </p>
+              <ProgressBarComponent
+                barSize="xxs"
+                barStriped={false}
+                barStyle="danger"
+                active={false}
+                valueNow={60}
+                valueMin={0}
+                valueMax={100}
+                screenReadersText={`${60}% Complete (warning)`}
+              />
+            </Panel>
+          </div>
         </div>
-      </div>
-      {/* source: */}
-      <div className="row">
-        <div className="col-xs-12">
-          <Panel
-            title="Source"
-            hasTitle={true}>
-            <Highlight className="javascript">
-              {source}
-            </Highlight>
-          </Panel>
+        {/* source: */}
+        <div className="row">
+          <div className="col-xs-12">
+            <Panel
+              title="Source"
+              hasTitle={true}>
+              <Highlight className="javascript">
+                {source}
+              </Highlight>
+            </Panel>
+          </div>
         </div>
-      </div>
-      </section>
+      </AnimatedView>
     );
   }
 }
