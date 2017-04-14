54ec101250325a525024e8e477d2696b577ccf20
MacKentoch <erwan.datin@gmail.com>
2017-04-14 05:13:20 +0200

    tests

---
+++ __tests__/components/tabPanel/tabPanelHeader/TabPanelHeader.test.js
@@ -0,0 +1,33 @@
+
+'use strict';
+
+import React                from 'react';
+import TabPanelHeader       from '../../../../src/app/components/tabPanel/TabPanelHeader/tabPanelHeader';
+import renderer             from 'react-test-renderer';
+
+describe('TabPanelHeader component', () => {
+  it('renders as expected', () => {
+    const mockProps = {
+      tabItems: [
+        {
+          name: 'item1',
+          tablink: '/item1',
+          isActive: true
+        },
+        {
+          name: 'item2',
+          tablink: '/item2',
+          isActive: false
+        }
+      ] 
+    };
+    const component = renderer.create(
+      <div>
+        <TabPanelHeader { ...mockProps }>
+          <p>TabPanelBody children here</p>
+        </TabPanelHeader>
+      </div>
+    ).toJSON();
+    expect(component).toMatchSnapshot();
+  });
+});
