88391783f7bf80612f2a99618192f9a6bdc2b288
MacKentoch <erwan.datin@gmail.com>
2017-04-17 06:26:07 +0200

    tests

---
+++ __tests__/components/teamMates/teamMates/TeamMates.test.js
@@ -0,0 +1,21 @@
+
+'use strict';
+
+import React              from 'react';
+import TeamMates          from '../../../../src/app/components/teamMates/teamMates/teamMates';
+import renderer           from 'react-test-renderer';
+
+describe('TeamMates component', () => {
+  it('renders as expected', () => {
+    const component = renderer.create(
+      <div>
+        <TeamMates>
+          <p>
+            TeamMates children here
+          </p>
+        </TeamMates>
+      </div>
+    ).toJSON();
+    expect(component).toMatchSnapshot();
+  });
+});
