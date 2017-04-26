4d1a6dd70c058fc549bdf063f471a62bf81f87e9
MacKentoch <erwan.datin@gmail.com>
2017-04-26 05:11:32 +0200

    tests

---
+++ __tests__/components/todoList/todoListCommands/TodoListSeeAllTask.test.js
@@ -0,0 +1,17 @@
+
+'use strict';
+
+import React              from 'react';
+import TodoListSeeAllTask   from '../../../../src/app/components/todoList/TodoListCommands/TodoListSeeAllTask';
+import renderer           from 'react-test-renderer';
+
+describe('TodoListSeeAllTask component', () => {
+  it('renders as expected', () => {
+    const component = renderer.create(
+      <div>
+        <TodoListSeeAllTask />
+      </div>
+    ).toJSON();
+    expect(component).toMatchSnapshot();
+  });
+});
