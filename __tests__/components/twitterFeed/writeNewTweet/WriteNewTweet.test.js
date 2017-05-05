c695c94b0f6a3592d50c4f3912e255241cb47262
MacKentoch <erwan.datin@gmail.com>
2017-05-05 05:29:52 +0200

    add flow types - test

---
+++ __tests__/components/twitterFeed/writeNewTweet/WriteNewTweet.test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+import React                from 'react';
+import WriteNewTweet        from '../../../../src/app/components/twitterFeed/writeNewTweet/WriteNewTweet';
+import renderer             from 'react-test-renderer';
+
+describe('WriteNewTweet component', () => {
+  it('renders as expected', () => {
+    const component = renderer.create(
+      <div>
+        <WriteNewTweet />>
+      </div>
+    ).toJSON();
+    expect(component).toMatchSnapshot();
+  });
+});
