7e49281b75687aefe13d9798e6adc9287edde288
MacKentoch <erwan.datin@gmail.com>
2017-06-28 05:10:05 +0200

    server

--- src/server/index.js
+++ src/server/index.js
@@ -2,9 +2,12 @@
 /* eslint-disable no-console */
 const express   = require('express');
 const path      = require('path');
+const chalk     = require('chalk');
 
 const app       = express();
 const DOCS_PATH = '../../docs/';
+const PORT      = 8082;
+const IP_ADRESS = 'localhost';
 
 app.use(express.static(path.join(__dirname, DOCS_PATH)));
 
@@ -13,8 +16,14 @@ app.get(
   (req, res) => res.sendFile(path.join(__dirname, DOCS_PATH, 'index.html'))
 );
 
+/* eslint-disable no-console */
 app.listen(
-  8081, 
-  'localhost', 
-  () => console.log('Minimalist production server running on localhost:8081')
+  PORT,
+  IP_ADRESS,
+  () => console.log(`
+    =====================================================
+    -> Server (${chalk.bgBlue('SPA')}) 🏃 (running) on ${chalk.green(IP_ADRESS)}:${chalk.green(PORT)}
+    =====================================================
+  `)
 );
+/* eslint-enable no-console */
