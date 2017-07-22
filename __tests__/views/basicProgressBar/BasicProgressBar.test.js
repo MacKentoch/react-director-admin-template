7e774e7ba8748cdc86a25f59de0c5a1a6e45a810
MacKentoch <erwan.datin@gmail.com>
2017-07-22 07:07:44 +0200

    test and theme scss

---
+++ __tests__/views/basicProgressBar/BasicProgressBar.test.js
@@ -0,0 +1,62 @@
+'use strict';
+
+import React            from 'react';
+import BasicProgressBar from '../../../src/app/views/basicProgressBar/BasicProgressBar';
+import renderer         from 'react-test-renderer'; // needed both for snpashot testing but also to prevent errors from enzyme
+import {
+  shallow
+}                       from 'enzyme';
+
+// react-highlight uses findDOMNode: 
+// -> jest will throw errors (no way to counter that...)
+jest.mock('react-highlight');
+
+describe('BasicProgressBar view', () => {
+  it('renders as expected', () => {
+    const props = {
+      actions: {
+        enterBasicProgressBar: () => {},
+        leaveBasicProgressBar: () => {}
+      }
+    };
+    const component = renderer.create(
+      <div>
+        <BasicProgressBar {...props} />
+      </div>
+    ).toJSON();
+    expect(component).toMatchSnapshot();
+  });
+
+
+  it('triggers enterBasicProgressBar on mount', () => {
+    const mockEnterBasicProgressBar = jest.fn();
+    const mockProps = {
+      actions: {
+        enterBasicProgressBar: mockEnterBasicProgressBar,
+        leaveBasicProgressBar: () => {}
+      }
+    };
+    /* eslint-disable no-unused-vars */
+    const wrapper = shallow(
+      <BasicProgressBar {...mockProps} />
+    );
+    // expect(mockEnterBasicProgressBar).toBeCalled();
+    expect(mockEnterBasicProgressBar.mock.calls.length).toBe(1);
+  });
+
+  it('triggers leaveBasicElements on unMount', () => {
+    const mockLeaveBasicProgressBar = jest.fn();
+    const mockProps = {
+      actions: {
+        enterBasicProgressBar: () => {},
+        leaveBasicProgressBar: mockLeaveBasicProgressBar
+      }
+    };
+    const wrapper = shallow(
+      <BasicProgressBar {...mockProps} />
+    );
+    wrapper.unmount();
+    // expect(mockLeaveBasicProgressBar).toBeCalled();
+    expect(mockLeaveBasicProgressBar.mock.calls.length).toBe(1);
+  });
+});
