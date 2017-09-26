f916f319a04f183f5b24db852be7e17c93ec4829
MacKentoch <erwan.datin@gmail.com>
2017-09-26 06:34:40 +0200

    tests

---
+++ __tests__/views/earningGraph/EarningGraph.test.js
@@ -0,0 +1,65 @@
+'use strict';
+
+import React            from 'react';
+import EarningGraph     from '../../../src/app/views/earningGraph/EarningGraph';
+import renderer         from 'react-test-renderer'; // needed both for snpashot testing but also to prevent errors from enzyme
+import {
+  shallow
+}                       from 'enzyme';
+import { 
+  MemoryRouter
+}                       from 'react-router';
+
+// react-highlight uses findDOMNode: 
+// -> jest will throw errors (no way to counter that...)
+jest.mock('react-highlight');
+
+describe('EarningGraph view', () => {
+  it('renders as expected', () => {
+    const props = {
+      actions: {
+        enterEarningGraph: () => {},
+        leaveEarningGraph: () => {}
+      }
+    };
+    const component = renderer.create(
+      <div>
+        <MemoryRouter>
+          <EarningGraph {...props} />
+        </MemoryRouter>
+      </div>
+    ).toJSON();
+    expect(component).toMatchSnapshot();
+  });
+
+
+  it('triggers enterEarningGraph on mount', () => {
+    const mockenterEarningGraph = jest.fn();
+    const mockProps = {
+      actions: {
+        enterEarningGraph: mockenterEarningGraph,
+        leaveEarningGraph: () => {}
+      }
+    };
+    /* eslint-disable no-unused-vars */
+    const wrapper = shallow(
+      <EarningGraph {...mockProps} />
+    );
+    expect(mockenterEarningGraph.mock.calls.length).toBe(1);
+  });
+
+  it('triggers leaveBasicElements on unMount', () => {
+    const mockleaveEarningGraph = jest.fn();
+    const mockProps = {
+      actions: {
+        enterEarningGraph: () => {},
+        leaveEarningGraph: mockleaveEarningGraph
+      }
+    };
+    const wrapper = shallow(
+      <EarningGraph {...mockProps} />
+    );
+    wrapper.unmount();
+    expect(mockleaveEarningGraph.mock.calls.length).toBe(1);
+  });
+});
