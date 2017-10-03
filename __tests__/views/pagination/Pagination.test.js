6bb9a8348efab4ad9ac5b338e4014c63274c5c2d
MacKentoch <erwan.datin@gmail.com>
2017-10-03 06:18:01 +0200

    tests

---
+++ __tests__/views/pagination/Pagination.test.js
@@ -0,0 +1,66 @@
+// @flow weak
+
+import React            from 'react';
+import Pagination       from '../../../src/app/views/pagination/Pagination';
+import renderer         from 'react-test-renderer'; // needed both for snpashot testing but also to prevent errors from enzyme
+import {
+  shallow
+}                       from 'enzyme';
+import { 
+  MemoryRouter
+}                       from 'react-router';
+
+// react-highlight uses findDOMNode: 
+// -> jest will throw errors (no way to counter that...)
+jest.mock('react-highlight');
+
+
+describe('Pagination view', () => {
+  it('renders as expected', () => {
+    const props = {
+      actions: {
+        enterPagination: () => {},
+        leaveEarningGraph: () => {}
+      }
+    };
+    const component = renderer.create(
+      <div>
+        <MemoryRouter>
+          <Pagination {...props} />
+        </MemoryRouter>
+      </div>
+    ).toJSON();
+    expect(component).toMatchSnapshot();
+  });
+
+
+  it('triggers enterPagination on mount', () => {
+    const mockenterPagination = jest.fn();
+    const mockProps = {
+      actions: {
+        enterPagination: mockenterPagination,
+        leavePagination: () => {}
+      }
+    };
+    /* eslint-disable no-unused-vars */
+    const wrapper = shallow(
+      <Pagination {...mockProps} />
+    );
+    expect(mockenterPagination.mock.calls.length).toBe(1);
+  });
+
+  it('triggers leavePagination on unMount', () => {
+    const mockleavePagination = jest.fn();
+    const mockProps = {
+      actions: {
+        enterPagination: () => {},
+        leavePagination: mockleavePagination
+      }
+    };
+    const wrapper = shallow(
+      <Pagination {...mockProps} />
+    );
+    wrapper.unmount();
+    expect(mockleavePagination.mock.calls.length).toBe(1);
+  });
+});
