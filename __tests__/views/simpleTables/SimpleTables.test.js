0db47891cf78ca6b788ac34f6e80a3d1ab12caa0
MacKentoch <erwan.datin@gmail.com>
2017-10-11 06:07:35 +0200

    Tests

---
+++ __tests__/views/simpleTables/SimpleTables.test.js
@@ -0,0 +1,65 @@
+// @flow weak
+
+import React            from 'react';
+import SimpleTables       from '../../../src/app/views/SimpleTables/SimpleTables';
+import renderer         from 'react-test-renderer'; // needed both for snpashot testing but also to prevent errors from enzyme
+import {
+  shallow
+}                       from 'enzyme';
+import { 
+  MemoryRouter
+}                       from 'react-router';
+
+// react-highlight uses findDOMNode: 
+// -> jest will throw errors (no way to counter that...)
+jest.mock('react-highlight');
+
+
+describe('SimpleTables view', () => {
+  it('renders as expected', () => {
+    const props = {
+      actions: {
+        enterSimpleTables: () => {},
+        leaveEarningGraph: () => {}
+      }
+    };
+    const component = renderer.create(
+      <div>
+        <MemoryRouter>
+          <SimpleTables {...props} />
+        </MemoryRouter>
+      </div>
+    ).toJSON();
+    expect(component).toMatchSnapshot();
+  });
+
+  it('triggers enterSimpleTables on mount', () => {
+    const mockenterSimpleTables = jest.fn();
+    const mockProps = {
+      actions: {
+        enterSimpleTables: mockenterSimpleTables,
+        leaveSimpleTables: () => {}
+      }
+    };
+    /* eslint-disable no-unused-vars */
+    const wrapper = shallow(
+      <SimpleTables {...mockProps} />
+    );
+    expect(mockenterSimpleTables.mock.calls.length).toBe(1);
+  });
+
+  it('triggers leaveSimpleTables on unMount', () => {
+    const mockleaveSimpleTables = jest.fn();
+    const mockProps = {
+      actions: {
+        enterSimpleTables: () => {},
+        leaveSimpleTables: mockleaveSimpleTables
+      }
+    };
+    const wrapper = shallow(
+      <SimpleTables {...mockProps} />
+    );
+    wrapper.unmount();
+    expect(mockleaveSimpleTables.mock.calls.length).toBe(1);
+  });
+});
