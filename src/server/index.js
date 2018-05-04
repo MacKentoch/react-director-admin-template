2266dfb0de01898de2e454b3a643c62cbf02ab92
Erwan DATIN <MacKentoch@users.noreply.github.com>
2018-05-04 06:54:53 +0200

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -10,7 +10,7 @@ const IP_ADRESS = 'localhost';
 app.use(express.static(path.join(__dirname, DOCS_PATH)));
 
 app.get(
-  '/',
+  '/*',
   (req, res) => res.sendFile(path.join(__dirname, DOCS_PATH, 'index.html'))
 );
 
