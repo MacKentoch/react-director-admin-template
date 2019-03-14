f466a194f4fc2dd43958850bb436ec44105adc6f
MacKentoch <erwan.datin@gmail.com>
2019-03-14 05:22:03 +0100

    migration

--- src/front/views/alert/Alert.js
+++ src/front/views/alert/Alert.js
@@ -1,115 +1,114 @@
-// @flow weak
+// @flow
+/* eslint-disable react/no-unescaped-entities */
 
-import React, { PureComponent } from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { AnimatedView, Panel, Alert as AlertComponent } from '../../components';
 import Highlight from 'react-highlight';
 
-class AlertView extends PureComponent {
-  static propTypes = {
-    actions: PropTypes.shape({
-      enterAlert: PropTypes.func.isRequired,
-      leaveAlert: PropTypes.func.isRequired,
-    }),
-  };
+type Props = {
+  actions: {
+    enterAlert: () => any,
+    leaveAlert: () => any,
+  },
+};
 
-  componentWillMount() {
-    const {
-      actions: { enterAlert },
-    } = this.props;
+const source = `
+// in render():
+<div className="row">
+  <div className="col-xs-12">
+    <Panel
+      title="Alert"
+      hasTitle={true}>
+      <Alert
+        type="danger">
+        <strong>
+          Oh snap!
+        </strong>
+        Change a few things up and try submitting again.
+      </Alert>
+
+      <Alert
+        type="success">
+        <strong>
+          Well done!
+        </strong>
+        You successfully read this important alert message.
+      </Alert>
+
+      <Alert
+        type="info">
+        <strong>
+          Heads up!
+        </strong>
+        This alert needs your attention, but it's not super important.
+      </Alert>
+
+      <Alert
+        type="warning">
+        <strong>
+          Warning!
+        </strong>
+        Best check yo self, you're not looking too good.
+      </Alert>
+    </Panel>
+  </div>
+</div>
+`;
+
+function AlertView({ actions: { enterAlert, leaveAlert } }: Props) {
+  useEffect(() => {
     enterAlert();
-  }
+    return () => leaveAlert();
+  }, []);
 
-  componentWillUnmount() {
-    const {
-      actions: { leaveAlert },
-    } = this.props;
-    leaveAlert();
-  }
-
-  render() {
-    const source = `
-      // in render():
+  return (
+    <AnimatedView>
+      {/* preview: */}
       <div className="row">
         <div className="col-xs-12">
-          <Panel
-            title="Alert"
-            hasTitle={true}>
-            <Alert
-              type="danger">
-              <strong>
-                Oh snap!
-              </strong>
+          <Panel title="Basic Progress Bars" hasTitle={true}>
+            <AlertComponent type="danger">
+              <strong>Oh snap!</strong>
               Change a few things up and try submitting again.
-            </Alert>
+            </AlertComponent>
 
-            <Alert
-              type="success">
-              <strong>
-                Well done!
-              </strong>
+            <AlertComponent type="success">
+              <strong>Well done!</strong>
               You successfully read this important alert message.
-            </Alert>
+            </AlertComponent>
 
-            <Alert
-              type="info">
-              <strong>
-                Heads up!
-              </strong>
+            <AlertComponent type="info">
+              <strong>Heads up!</strong>
               This alert needs your attention, but it's not super important.
-            </Alert>
+            </AlertComponent>
 
-            <Alert
-              type="warning">
-              <strong>
-                Warning!
-              </strong>
+            <AlertComponent type="warning">
+              <strong>Warning!</strong>
               Best check yo self, you're not looking too good.
-            </Alert>
+            </AlertComponent>
           </Panel>
         </div>
       </div>
-      `;
-
-    return (
-      <AnimatedView>
-        {/* preview: */}
-        <div className="row">
-          <div className="col-xs-12">
-            <Panel title="Basic Progress Bars" hasTitle={true}>
-              <AlertComponent type="danger">
-                <strong>Oh snap!</strong>
-                Change a few things up and try submitting again.
-              </AlertComponent>
-
-              <AlertComponent type="success">
-                <strong>Well done!</strong>
-                You successfully read this important alert message.
-              </AlertComponent>
-
-              <AlertComponent type="info">
-                <strong>Heads up!</strong>
-                This alert needs your attention, but it's not super important.
-              </AlertComponent>
-
-              <AlertComponent type="warning">
-                <strong>Warning!</strong>
-                Best check yo self, you're not looking too good.
-              </AlertComponent>
-            </Panel>
-          </div>
+      {/* source: */}
+      <div className="row">
+        <div className="col-xs-12">
+          <Panel title="Source" hasTitle={true}>
+            <Highlight className="javascript">{source}</Highlight>
+          </Panel>
         </div>
-        {/* source: */}
-        <div className="row">
-          <div className="col-xs-12">
-            <Panel title="Source" hasTitle={true}>
-              <Highlight className="javascript">{source}</Highlight>
-            </Panel>
-          </div>
-        </div>
-      </AnimatedView>
-    );
-  }
+      </div>
+    </AnimatedView>
+  );
 }
+
+AlertView.displayName = 'AlertView';
+
+AlertView.propTypes = {
+  actions: PropTypes.shape({
+    enterAlert: PropTypes.func.isRequired,
+    leaveAlert: PropTypes.func.isRequired,
+  }),
+};
 
 export default AlertView;
