6542f967d431afc8b1e6fe49b10329ae5b52242b
MacKentoch <erwan.datin@gmail.com>
2019-03-26 05:21:47 +0100

    migration

--- src/front/components/stat/Stat.js
+++ src/front/components/stat/Stat.js
@@ -1,44 +1,45 @@
-// @flow weak
+// @flow
 /* eslint consistent-return: 0 */
 
-import React      from 'react';
-import PropTypes  from 'prop-types';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+type Props = {
+  statFaIconName?: string,
+  statIconColor?: string,
+  statLabel: string,
+  statIconSpin: boolean,
+};
 
 const Stat = ({
-  statFaIconName,
-  statIconColor,
-  statLabel,
-  statIconSpin
-}) => (
+  statFaIconName = '',
+  statIconColor = '#F1F1F1',
+  statLabel = '',
+  statIconSpin = false,
+}: Props) => (
   <div className="stat">
-    <div style={{textAlign: 'center'}}>
+    <div style={{ textAlign: 'center' }}>
       <span className="fa-stack fa-lg fa-3x">
+        <i className="fa fa-circle fa-stack-2x" style={{ color: '#FFFFFF' }} />
         <i
-          className="fa fa-circle fa-stack-2x"
-          style={{color: '#FFFFFF'}}>
-        </i>
-        <i
-          className={`fa ${statFaIconName} ${statIconSpin ? 'fa-spin' : ''} fa-stack-1x fa-inverse`}
-          style={{color: statIconColor}}>
-        </i>
+          className={`fa ${statFaIconName} ${
+            statIconSpin ? 'fa-spin' : ''
+          } fa-stack-1x fa-inverse`}
+          style={{ color: statIconColor }}
+        />
       </span>
     </div>
-    <h5 className="stat-info">
-      {statLabel}
-    </h5>
+    <h5 className="stat-info">{statLabel}</h5>
   </div>
 );
 
 Stat.propTypes = {
   statFaIconName: PropTypes.string.isRequired,
-  statIconColor:  PropTypes.string,
-  statLabel:      PropTypes.string.isRequired,
-  statIconSpin:   PropTypes.bool.isRequired
+  statIconColor: PropTypes.string,
+  statLabel: PropTypes.string.isRequired,
+  statIconSpin: PropTypes.bool.isRequired,
 };
 
-Stat.defaultProps = {
-  statColor:    '4A4A4A',
-  statIconSpin: false
-};
+Stat.displayName = 'Stat';
 
 export default Stat;
--- src/front/components/statsCard/StatsCard.js
+++ src/front/components/statsCard/StatsCard.js
@@ -1,46 +1,37 @@
-// @flow weak
+// @flow
 
-import React      from 'react';
-import PropTypes  from 'prop-types';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+type Props = {
+  statValue: string,
+  statLabel: string,
+  icon: any,
+  backColor: 'red' | 'blue' | 'violet' | 'green',
+};
 
 const StatsCard = ({
-  statValue,
-  statLabel,
-  icon,
-  backColor
-}) => (
+  statValue = '0',
+  statLabel = 'unknown',
+  icon = <i className="fa fa-check-square-o" />,
+  backColor = 'blue',
+}: Props) => (
   <div className="sm-st clearfix">
-    <span className={ `sm-st-icon st-${backColor}` }>
-      { icon }
-    </span>
+    <span className={`sm-st-icon st-${backColor}`}>{icon}</span>
     <div className="sm-st-info">
-      <span>
-        { statValue }
-      </span>
-      <div>
-        { statLabel }
-      </div>
+      <span>{statValue}</span>
+      <div>{statLabel}</div>
     </div>
   </div>
 );
 
-StatsCard.propTypes = {
-  statValue:  PropTypes.string,
-  statLabel:  PropTypes.string,
-  icon:       PropTypes.node,
-  backColor:  PropTypes.oneOf([
-    'red',
-    'blue',
-    'violet',
-    'green'
-  ])
-};
+StatsCard.displayName = StatsCard;
 
-StatsCard.defaultProps = {
-  statValue:  '0',
-  statLabel:  'unknown',
-  icon:       (<i className="fa fa-check-square-o"></i>),
-  backColor:  'blue'
+StatsCard.propTypes = {
+  statValue: PropTypes.string,
+  statLabel: PropTypes.string,
+  icon: PropTypes.node,
+  backColor: PropTypes.oneOf(['red', 'blue', 'violet', 'green']),
 };
 
 export default StatsCard;
