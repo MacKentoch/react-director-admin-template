6ab529d38daaa078339ebcf32f56e40432bb26dd
MacKentoch <erwan.datin@gmail.com>
2019-04-01 05:14:40 +0200

    migration

--- src/front/components/textInput/TelephoneInput.js
+++ src/front/components/textInput/TelephoneInput.js
@@ -1,108 +1,74 @@
-// @flow weak
+// @flow
 
-import React, {
-  PureComponent
-}                     from 'react';
-import PropTypes      from 'prop-types';
-import cx             from 'classnames';
+import React, { useState, useCallback, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+
+type Props = {
+  label: string,
+  id: string | number,
+  value: string,
+  onChange: (value: string) => any,
+  delay: number,
+};
 
 const telephoneRegex = /^(\+33|0033|0)([0-9])[0-9]{8}$/g;
 
-class TelephoneInput extends PureComponent {
-  static propTypes = {
-    label:    PropTypes.string.isRequired,
-    id:       PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
-    value:    PropTypes.string.isRequired,
-    onChange: PropTypes.func.isRequired,
-    delay:    PropTypes.number
-  };
+function TelephoneInput({ label, id, value, onChange, delay = 200 }: Props) {
+  const [valid, setValid] = useState(true);
+  const [stateValue, setStateValue] = useState('');
 
-  static defaultProps = {
-    delay: 200
-  };
-
-  state = {
-    valid:      true,
-    stateValue: ''
-  };
-
-  timer = null;
-
-  componentWillReceiveProps(nextProps) {
-    const { stateValue } = this.state;
-    const { value } = nextProps;
-
-    if ((value !== stateValue) && stateValue.length === 0) {
-      this.setState({stateValue: value});
+  useEffect(() => {
+    if (value !== stateValue && stateValue.length === 0) {
+      setStateValue(value);
     }
-  }
+  }, [stateValue, value]);
 
-  componentWillUnmount() {
-    if (this.timer) {
-      clearTimeout(this.timer);
-      this.timer = null;
-    }
-  }
+  const handlesOnChange = useCallback((event?: SyntheticEvent<>) => {
+    event && event.preventDefault();
 
-  render() {
-    const {label, id} = this.props;
-    const {valid} = this.state;
-    const { stateValue } = this.state;
+    // $FlowIgnore
+    const newValue = event.target.value;
+    const isValid = telephoneRegex.test(newValue);
 
-    return (
-      <div
-        className={
-          cx({
-            'form-group': true,
-            'has-error': !valid
-          })
-        }>
-        <label
-          className="control-label"
-          htmlFor={id}>
-          {label}
-        </label>
-        <div>
-          <input
-            className="form-control"
-            id={id}
-            type="text"
-            // value={value}
-            defaultValue={stateValue}
-            // onChange={this.handlesOnChange} // IE11 misses some keys entered... yes I know what you think...
-            onInput={this.handlesOnChange}
-          />
-        </div>
+    setValid(isValid);
+    setStateValue(newValue);
+    onChange(newValue);
+  }, []);
+
+  return (
+    <div
+      className={cx({
+        'form-group': true,
+        'has-error': !valid,
+      })}
+    >
+      <label className="control-label" htmlFor={id}>
+        {label}
+      </label>
+      <div>
+        <input
+          className="form-control"
+          id={id}
+          type="text"
+          // value={value}
+          defaultValue={stateValue}
+          // onChange={this.handlesOnChange} // IE11 misses some keys entered... yes I know what you think...
+          onInput={handlesOnChange}
+        />
       </div>
-    );
-  }
-
-  checkIsValidTelephone(value) {
-    // test telephone pattern:
-    const isValid = telephoneRegex.test(value);
-    this.setState({ valid: isValid });
-  }
-
-  handlesOnChange = (event) => {
-    event.preventDefault();
-    this.setState({stateValue: event.target.value});
-    this.checkIsValidTelephone(event.target.value);
-    this.setTimerBeforeCallback(event.target.value);
-  }
-  // hack to prevent bad user xp when huge forms and callback each onChange to parent or store like redux:
-  setTimerBeforeCallback(value) {
-    const { onChange, delay } = this.props;
-
-    if (this.timer) {
-      clearTimeout(this.timer);
-      this.timer = null;
-    }
-
-    this.timer = setTimeout(
-      () => onChange(value),
-      delay
-    );
-  }
+    </div>
+  );
 }
+
+TelephoneInput.displayName = 'TelephoneInput';
+
+TelephoneInput.propTypes = {
+  label: PropTypes.string.isRequired,
+  id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  delay: PropTypes.number,
+};
 
 export default TelephoneInput;
