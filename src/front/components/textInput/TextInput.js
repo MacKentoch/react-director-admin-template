30337ae7e8ced1ddba78de49ea691e4ae820bfcd
MacKentoch <erwan.datin@gmail.com>
2019-04-03 05:48:55 +0200

    migration

--- src/front/components/textInput/TextInput.js
+++ src/front/components/textInput/TextInput.js
@@ -1,88 +1,60 @@
-// @flow weak
+// @flow
 
-import React, {
-  PureComponent
-}                     from 'react';
-import PropTypes      from 'prop-types';
+import React, { useState, useEffect, useCallback } from 'react';
+import PropTypes from 'prop-types';
 
-class TextInput extends PureComponent {
-  static propTypes = {
-    label:    PropTypes.string.isRequired,
-    id:       PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
-    value:    PropTypes.string.isRequired,
-    onChange: PropTypes.func.isRequired,
-    delay:    PropTypes.number
-  };
+type Props = {
+  label: string,
+  id: string | number,
+  value: string,
+  onChange: (value: string) => any,
+};
 
-  static defaultProps = {
-    delay: 200
-  };
+function TextInput({ label, id, value, onChange }: Props) {
+  const [stateValue, setStateValue] = useState('');
 
-  state = { stateValue: '' };
-
-  timer = null;
-
-  componentWillReceiveProps(nextProps) {
-    const { stateValue } = this.state;
-    const { value } = nextProps;
-
-    if ((value !== stateValue) && stateValue.length === 0) {
-      this.setState({stateValue: value});
+  useEffect(() => {
+    if (value !== stateValue && stateValue.length === 0) {
+      setStateValue(value);
     }
-  }
+  }, [stateValue, value]);
 
-  componentWillUnmount() {
-    if (this.timer) {
-      clearTimeout(this.timer);
-      this.timer = null;
-    }
-  }
+  const handlesOnChange = useCallback((event?: SyntheticEvent<>) => {
+    event && event.preventDefault();
 
-  render() {
-    const {label, id} = this.props;
-    const { stateValue } = this.state;
+    // $FlowIgnore
+    const newValue = event.target.value;
+    setStateValue(newValue);
+    onChange(newValue);
+  }, []);
 
-    return (
-      <div className="form-group">
-        <label
-          className="control-label"
-          htmlFor={id}>
-          {label}
-        </label>
-        <div>
-          <input
-            className="form-control"
-            id={id}
-            type="text"
-            // value={value}
-            defaultValue={stateValue}
-            // onChange={this.handlesOnChange} // IE11 misses some keys entered... yes I know what you think...
-            onInput={this.handlesOnChange}
-          />
-        </div>
+  return (
+    <div className="form-group">
+      <label className="control-label" htmlFor={id}>
+        {label}
+      </label>
+      <div>
+        <input
+          className="form-control"
+          id={id}
+          type="text"
+          // value={value}
+          defaultValue={stateValue}
+          // onChange={this.handlesOnChange} // IE11 misses some keys entered... yes I know what you think...
+          onInput={handlesOnChange}
+        />
       </div>
-    );
-  }
-
-  handlesOnChange = (event) => {
-    event.preventDefault();
-    this.setState({stateValue: event.target.value});
-    this.setTimerBeforeCallback(event.target.value);
-  }
-  // hack to prevent bad user xp when huge forms and callback each onChange to parent or store like redux:
-  setTimerBeforeCallback(value) {
-    const { onChange, delay } = this.props;
-
-    if (this.timer) {
-      clearTimeout(this.timer);
-      this.timer = null;
-    }
-
-    this.timer = setTimeout(
-      () => onChange(value),
-      delay
-    );
-  }
+    </div>
+  );
 }
+
+TextInput.displayName = 'TextInput';
+
+TextInput.propTypes = {
+  label: PropTypes.string.isRequired,
+  id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
 
 export default TextInput;
