6ca8efa952e4532d59517418609bbaede61757d4
MacKentoch <erwan.datin@gmail.com>
2019-04-25 05:23:45 +0200

    migration

--- src/front/views/pagination/Pagination.js
+++ src/front/views/pagination/Pagination.js
@@ -1,165 +1,147 @@
-// @flow weak
+// @flow
 
-import React, {
-  PureComponent
-}                         from 'react';
-import PropTypes          from 'prop-types';
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
 import {
   AnimatedView,
   Panel,
   Pagination as PaginationComponent,
-  Pager as PagerComponent
-}                         from '../../components';
-import Highlight          from 'react-highlight';
+  Pager as PagerComponent,
+} from '../../components';
+import Highlight from 'react-highlight';
 
+type Props = {
+  actions: {
+    enterPagination: () => any,
+    leavePagination: () => any,
+  },
+};
 
-class PaginationView extends PureComponent {
-  static propTypes = {
-    actions: PropTypes.shape({
-      enterPagination: PropTypes.func.isRequired,
-      leavePagination: PropTypes.func.isRequired
-    })
-  };
+const source = `
+  // in render():
+  <div className="text-center">
+    <Pagination
+      size="large"
+      numberOfPagination={5}
+    />
+  </div>
+  <div className="text-center">
+    <Pagination
+      numberOfPagination={5}
+    />
+  </div>
+  <div className="text-center">
+    <Pagination
+      size="small"
+      numberOfPagination={5}
+    />
+  </div>
+  <div>
+    <h3>Default Example</h3>
+    <Pager
+      aligned={false}
+      previous={'Previous'}
+      next={'Next'}
+    />
+  </div>
+  <div>
+    <h3>Aligned links</h3>
+    <Pager
+      aligned={true}
+      previous={
+        <div>
+          <span aria-hidden="true">
+            &larr;
+          </span>
+          Older
+        </div>
+      }
+      next={(
+        <div>
+          Newer
+          <span aria-hidden="true">
+            &rarr;
+          </span>
+        </div>
+      )}
+    />
+  </div>
+`;
 
-  componentWillMount() {
-    const { actions: { enterPagination } } = this.props;
+function PaginationView({
+  actions: { enterPagination, leavePagination },
+}: Props) {
+  useEffect(() => {
     enterPagination();
-  }
 
-  componentWillUnmount() {
-    const { actions: { leavePagination } } = this.props;
-    leavePagination();
-  }
+    return () => {
+      leavePagination();
+    };
+  }, []);
 
-  render() {
-    const source = `
-      // in render():
-      <div className="text-center">
-        <Pagination
-          size="large"
-          numberOfPagination={5}
-        />
-      </div>
-      <div className="text-center">
-        <Pagination
-          numberOfPagination={5}
-        />
-      </div>
-      <div className="text-center">
-        <Pagination
-          size="small"
-          numberOfPagination={5}
-        />
-      </div>
-      <div>
-        <h3>Default Example</h3>
-        <Pager
-          aligned={false}
-          previous={'Previous'}
-          next={'Next'}
-        />
-      </div>
-      <div>
-        <h3>Aligned links</h3>
-        <Pager
-          aligned={true}
-          previous={
-            <div>
-              <span aria-hidden="true">
-                &larr;
-              </span>
-              Older
+  return (
+    <AnimatedView>
+      {/* preview: */}
+      <div className="row">
+        <div className="col-xs-12">
+          <Panel title="Pagination" hasTitle={true}>
+            <div className="text-center">
+              <PaginationComponent size="large" numberOfPagination={5} />
             </div>
-          }
-          next={(
-            <div>
-              Newer
-              <span aria-hidden="true">
-                &rarr;
-              </span>
+            <div className="text-center">
+              <PaginationComponent numberOfPagination={5} />
             </div>
-          )}
-        />
+            <div className="text-center">
+              <PaginationComponent size="small" numberOfPagination={5} />
+            </div>
+            <div>
+              <h3>Default Example</h3>
+              <PagerComponent
+                aligned={false}
+                previous={'Previous'}
+                next={'Next'}
+              />
+            </div>
+            <div>
+              <h3>Aligned links</h3>
+              <PagerComponent
+                aligned={true}
+                previous={
+                  <div>
+                    <span aria-hidden="true">&larr;</span>
+                    Older
+                  </div>
+                }
+                next={
+                  <div>
+                    Newer
+                    <span aria-hidden="true">&rarr;</span>
+                  </div>
+                }
+              />
+            </div>
+          </Panel>
+        </div>
       </div>
-      `;
-
-    return(
-      <AnimatedView>
-        {/* preview: */}
-        <div className="row">
-          <div className="col-xs-12">
-            <Panel
-              title="Pagination"
-              hasTitle={true}>
-              <div className="text-center">
-                <PaginationComponent
-                  size="large"
-                  numberOfPagination={5}
-                />
-              </div>
-              <div className="text-center">
-                <PaginationComponent
-                  numberOfPagination={5}
-                />
-              </div>
-              <div className="text-center">
-                <PaginationComponent
-                  size="small"
-                  numberOfPagination={5}
-                />
-              </div>
-              <div>
-                <h3>
-                  Default Example
-                </h3>
-                <PagerComponent
-                  aligned={false}
-                  previous={'Previous'}
-                  next={'Next'}
-                />
-              </div>
-              <div>
-                <h3>
-                  Aligned links
-                </h3>
-                <PagerComponent
-                  aligned={true}
-                  previous={
-                    <div>
-                      <span aria-hidden="true">
-                        &larr;
-                      </span>
-                      Older
-                    </div>
-                  }
-                  next={(
-                    <div>
-                      Newer
-                      <span aria-hidden="true">
-                        &rarr;
-                      </span>
-                    </div>
-                  )}
-                />
-              </div>
-            </Panel>
-          </div>
+      {/* source: */}
+      <div className="row">
+        <div className="col-xs-12">
+          <Panel title="Source" hasTitle={true}>
+            <Highlight className="javascript">{source}</Highlight>
+          </Panel>
         </div>
-        {/* source: */}
-        <div className="row">
-          <div className="col-xs-12">
-            <Panel
-              title="Source"
-              hasTitle={true}>
-              <Highlight className="javascript">
-                {source}
-              </Highlight>
-            </Panel>
-          </div>
-        </div>
-      </AnimatedView>
-    );
-  }
+      </div>
+    </AnimatedView>
+  );
 }
+
+PaginationView.displayName = 'PaginationView';
+
+PaginationView.propTypes = {
+  actions: PropTypes.shape({
+    enterPagination: PropTypes.func.isRequired,
+    leavePagination: PropTypes.func.isRequired,
+  }),
+};
 
 export default PaginationView;
