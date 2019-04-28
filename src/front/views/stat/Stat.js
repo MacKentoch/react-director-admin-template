4280e1f56e6b0fd0a2ec6653e0b9ea6d8f87ba52
MacKentoch <erwan.datin@gmail.com>
2019-04-28 07:24:19 +0200

    migration

--- src/front/views/stat/Stat.js
+++ src/front/views/stat/Stat.js
@@ -1,162 +1,151 @@
-// @flow weak
+// @flow
 
-import React, {
-  PureComponent
-}                         from 'react';
-import PropTypes          from 'prop-types';
-import {
-  AnimatedView,
-  Panel,
-  Stat as StatComponent
-}                         from '../../components';
-import Highlight          from 'react-highlight';
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { AnimatedView, Panel, Stat as StatComponent } from '../../components';
+import Highlight from 'react-highlight';
 
+type Props = {
+  actions: {
+    enterStat: () => {},
+    leaveStat: () => {},
+  },
+};
 
-class Stat extends PureComponent {
-  componentWillMount() {
-    const { actions: { enterStat } } = this.props;
+const source = `
+  // import
+  import { Stat } from './_SOMEWHERE_/components';
+
+  // in render():
+  <div className="row">
+    <div className="col-md-2">
+      <Stat
+        statFaIconName="fa-file-o"
+        statIconColor="#fa8564"
+        statLabel="999 Projects"
+      />
+    </div>
+    <div className="col-md-2">
+      <Stat
+        statFaIconName="fa-paperclip"
+        statIconColor="#45cf95"
+        statLabel="999 Documents"
+      />
+    </div>
+    <div className="col-md-2">
+      <Stat
+        statFaIconName="fa-envelope-o"
+        statIconColor="#AC75F0"
+        statLabel="999 Messages"
+      />
+    </div>
+    <div className="col-md-2">
+      <Stat
+        statFaIconName="fa-check-square-o"
+        statIconColor="#45cf95"
+        statLabel="1000 Tasks"
+      />
+    </div>
+    <div className="col-md-2">
+      <Stat
+        statFaIconName="fa-dollar"
+        statIconColor="#AC75F0"
+        statLabel="$99999 Earnings"
+      />
+    </div>
+    <div className="col-md-2">
+      <Stat
+        statFaIconName="fa-refresh"
+        statIconColor="#fa8564"
+        statIconSpin={true}
+        statLabel="Processing...."
+      />
+    </div>
+  </div>
+`;
+
+function Stat({ actions: { enterStat, leaveStat } }: Props) {
+  useEffect(() => {
     enterStat();
-  }
 
-  componentWillUnmount() {
-    const { actions: { leaveStat } } = this.props;
-    leaveStat();
-  }
+    return () => {
+      leaveStat();
+    };
+  }, []);
 
-  render() {
-    const source = `
-      // import
-      import { Stat } from './_SOMEWHERE_/components';
-
-      // in render():
+  return (
+    <AnimatedView>
+      {/* preview: */}
       <div className="row">
-        <div className="col-md-2">
-          <Stat
-            statFaIconName="fa-file-o"
-            statIconColor="#fa8564"
-            statLabel="999 Projects"
-          />
-        </div>
-        <div className="col-md-2">
-          <Stat
-            statFaIconName="fa-paperclip"
-            statIconColor="#45cf95"
-            statLabel="999 Documents"
-          />
-        </div>
-        <div className="col-md-2">
-          <Stat
-            statFaIconName="fa-envelope-o"
-            statIconColor="#AC75F0"
-            statLabel="999 Messages"
-          />
-        </div>
-        <div className="col-md-2">
-          <Stat
-            statFaIconName="fa-check-square-o"
-            statIconColor="#45cf95"
-            statLabel="1000 Tasks"
-          />
-        </div>
-        <div className="col-md-2">
-          <Stat
-            statFaIconName="fa-dollar"
-            statIconColor="#AC75F0"
-            statLabel="$99999 Earnings"
-          />
-        </div>
-        <div className="col-md-2">
-          <Stat
-            statFaIconName="fa-refresh"
-            statIconColor="#fa8564"
-            statIconSpin={true}
-            statLabel="Processing...."
-          />
+        <div className="col-xs-12">
+          <Panel title="Stat" hasTitle={true} bodyBackGndColor={'#F4F5F6'}>
+            <div className="row">
+              <div className="col-md-2">
+                <StatComponent
+                  statFaIconName="fa-file-o"
+                  statIconColor="#fa8564"
+                  statLabel="999 Projects"
+                />
+              </div>
+              <div className="col-md-2">
+                <StatComponent
+                  statFaIconName="fa-paperclip"
+                  statIconColor="#45cf95"
+                  statLabel="999 Documents"
+                />
+              </div>
+              <div className="col-md-2">
+                <StatComponent
+                  statFaIconName="fa-envelope-o"
+                  statIconColor="#AC75F0"
+                  statLabel="999 Messages"
+                />
+              </div>
+              <div className="col-md-2">
+                <StatComponent
+                  statFaIconName="fa-check-square-o"
+                  statIconColor="#45cf95"
+                  statLabel="1000 Tasks"
+                />
+              </div>
+              <div className="col-md-2">
+                <StatComponent
+                  statFaIconName="fa-dollar"
+                  statIconColor="#AC75F0"
+                  statLabel="$99999 Earnings"
+                />
+              </div>
+              <div className="col-md-2">
+                <StatComponent
+                  statFaIconName="fa-refresh"
+                  statIconColor="#fa8564"
+                  statIconSpin={true}
+                  statLabel="Processing...."
+                />
+              </div>
+            </div>
+          </Panel>
         </div>
       </div>
-      `;
-
-    return(
-      <AnimatedView>
-        {/* preview: */}
-        <div className="row">
-          <div className="col-xs-12">
-            <Panel
-              title="Stat"
-              hasTitle={true}
-              bodyBackGndColor={'#F4F5F6'}>
-
-              <div className="row">
-                <div className="col-md-2">
-                  <StatComponent
-                    statFaIconName="fa-file-o"
-                    statIconColor="#fa8564"
-                    statLabel="999 Projects"
-                  />
-                </div>
-                <div className="col-md-2">
-                  <StatComponent
-                    statFaIconName="fa-paperclip"
-                    statIconColor="#45cf95"
-                    statLabel="999 Documents"
-                  />
-                </div>
-                <div className="col-md-2">
-                  <StatComponent
-                    statFaIconName="fa-envelope-o"
-                    statIconColor="#AC75F0"
-                    statLabel="999 Messages"
-                  />
-                </div>
-                <div className="col-md-2">
-                  <StatComponent
-                    statFaIconName="fa-check-square-o"
-                    statIconColor="#45cf95"
-                    statLabel="1000 Tasks"
-                  />
-                </div>
-                <div className="col-md-2">
-                  <StatComponent
-                    statFaIconName="fa-dollar"
-                    statIconColor="#AC75F0"
-                    statLabel="$99999 Earnings"
-                  />
-                </div>
-                <div className="col-md-2">
-                  <StatComponent
-                    statFaIconName="fa-refresh"
-                    statIconColor="#fa8564"
-                    statIconSpin={true}
-                    statLabel="Processing...."
-                  />
-                </div>
-              </div>
-
-            </Panel>
-          </div>
+      {/* source: */}
+      <div className="row">
+        <div className="col-xs-12">
+          <Panel title="Source" hasTitle={true}>
+            <Highlight className="javascript">{source}</Highlight>
+          </Panel>
         </div>
-        {/* source: */}
-        <div className="row">
-          <div className="col-xs-12">
-            <Panel
-              title="Source"
-              hasTitle={true}>
-              <Highlight className="javascript">
-                {source}
-              </Highlight>
-            </Panel>
-          </div>
-        </div>
-      </AnimatedView>
-    );
-  }
+      </div>
+    </AnimatedView>
+  );
 }
 
-Stat.propTypes= {
+Stat.displayName = 'Stat';
+
+Stat.propTypes = {
   actions: PropTypes.shape({
     enterStat: PropTypes.func.isRequired,
-    leaveStat: PropTypes.func.isRequired
-  })
+    leaveStat: PropTypes.func.isRequired,
+  }),
 };
 
 export default Stat;
