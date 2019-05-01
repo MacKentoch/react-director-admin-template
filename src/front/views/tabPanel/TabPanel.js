f6055f6238b02ad24b43656af537d7f4edc59b06
MacKentoch <erwan.datin@gmail.com>
2019-05-01 06:44:32 +0200

    migration

--- src/front/views/tabPanel/TabPanel.js
+++ src/front/views/tabPanel/TabPanel.js
@@ -1,23 +1,37 @@
-// @flow weak
+// @flow
 
-import React, {
-  PureComponent
-}                         from 'react';
-import PropTypes          from 'prop-types';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import {
   AnimatedView,
   Panel,
-  TabPanel            as TabPanelComponent,
-  TabPanelHeader      as TabPanelHeaderComponent,
-  TabPanelBody        as TabPanelBodyComponent,
-  TabPanelBodyContent as TabPanelBodyContentComponent
-}                         from '../../components';
-import Highlight          from 'react-highlight';
+  TabPanel as TabPanelComponent,
+  TabPanelHeader as TabPanelHeaderComponent,
+  TabPanelBody as TabPanelBodyComponent,
+  TabPanelBodyContent as TabPanelBodyContentComponent,
+} from '../../components';
+import Highlight from 'react-highlight';
+import { type RouterProps } from '../../types/react-router';
 
+type Props = {
+  actions: {
+    enterTabPanel: () => any,
+    leaveTabPanel: () => any,
+  },
+} & RouterProps;
 
-class TabPanel extends PureComponent {
+const source = `
+  // import
+  import {
+    TabPanel,
+    TabPanelHeader,
+    TabPanelBody,
+    TabPanelBodyContent
+  } from './_SOMEWHERE_/components';
+
+  // tab headers (in state for example):
   state = {
-    mockHeader: [
+    headers: [
       {name: 'Home', tablink: 'home', isActive: true},
       {name: 'About', tablink: 'about', isActive: false},
       {name: 'Profile', tablink: 'profile', isActive: false},
@@ -25,109 +39,81 @@ class TabPanel extends PureComponent {
     ]
   };
 
-  componentWillMount() {
-    const { actions: { enterTabPanel } } = this.props;
+  // in render():
+    <TabPanel>
+      <TabPanelHeader tabItems={this.state.headers}/>
+      <TabPanelBody>
+        <TabPanelBodyContent id="home">
+          &nbsp;Home
+        </TabPanelBodyContent>
+        <TabPanelBodyContent id="about">
+          &nbsp;About
+        </TabPanelBodyContent>
+        <TabPanelBodyContent id="profile">
+          &nbsp;Profile
+        </TabPanelBodyContent>
+      </TabPanelBody>
+    </TabPanel>
+`;
+
+function TabPanel({ actions: { enterTabPanel, leaveTabPanel } }: Props) {
+  const [mockHeader] = useState([
+    { name: 'Home', tablink: 'home', isActive: true },
+    { name: 'About', tablink: 'about', isActive: false },
+    { name: 'Profile', tablink: 'profile', isActive: false },
+    { name: 'Contact', tablink: 'contact', isActive: false },
+  ]);
+
+  useEffect(() => {
     enterTabPanel();
-  }
 
-  componentWillUnmount() {
-    const { actions: { leaveTabPanel } } = this.props;
-    leaveTabPanel();
-  }
+    return () => {
+      leaveTabPanel();
+    };
+  }, []);
 
-  render() {
-    const { mockHeader } = this.state;
-
-    const source = `
-      // import
-      import {
-        TabPanel,
-        TabPanelHeader,
-        TabPanelBody,
-        TabPanelBodyContent
-      } from './_SOMEWHERE_/components';
-
-      // tab headers (in state for example):
-      state = {
-        headers: [
-          {name: 'Home', tablink: 'home', isActive: true},
-          {name: 'About', tablink: 'about', isActive: false},
-          {name: 'Profile', tablink: 'profile', isActive: false},
-          {name: 'Contact', tablink: 'contact', isActive: false}
-        ]
-      };
-
-      // in render():
-        <TabPanel>
-          <TabPanelHeader tabItems={this.state.headers}/>
-          <TabPanelBody>
-            <TabPanelBodyContent id="home">
-              &nbsp;Home
-            </TabPanelBodyContent>
-            <TabPanelBodyContent id="about">
-              &nbsp;About
-            </TabPanelBodyContent>
-            <TabPanelBodyContent id="profile">
-              &nbsp;Profile
-            </TabPanelBodyContent>
-          </TabPanelBody>
-        </TabPanel>
-      `;
-
-    return(
-      <AnimatedView>
-        {/* preview: */}
-        <div className="row">
-          <div className="col-xs-6 col-xs-offset-3">
-            <Panel
-              title="TabPanel"
-              hasTitle={true}
-              bodyBackGndColor={'#F4F5F6'}>
-              <TabPanelComponent>
-                <TabPanelHeaderComponent tabItems={mockHeader}/>
-                <TabPanelBodyComponent>
-                  <TabPanelBodyContentComponent id="home" isActive>
-                    <h3>
-                      Home
-                    </h3>
-                  </TabPanelBodyContentComponent>
-                  <TabPanelBodyContentComponent id="about">
-                    <h3>
-                      About
-                    </h3>
-                  </TabPanelBodyContentComponent>
-                  <TabPanelBodyContentComponent id="profile">
-                    <h3>
-                      Profile
-                    </h3>
-                  </TabPanelBodyContentComponent>
-                </TabPanelBodyComponent>
-              </TabPanelComponent>
-            </Panel>
-          </div>
+  return (
+    <AnimatedView>
+      {/* preview: */}
+      <div className="row">
+        <div className="col-xs-6 col-xs-offset-3">
+          <Panel title="TabPanel" hasTitle bodyBackGndColor={'#F4F5F6'}>
+            <TabPanelComponent>
+              <TabPanelHeaderComponent tabItems={mockHeader} />
+              <TabPanelBodyComponent>
+                <TabPanelBodyContentComponent id="home" isActive>
+                  <h3>Home</h3>
+                </TabPanelBodyContentComponent>
+                <TabPanelBodyContentComponent id="about">
+                  <h3>About</h3>
+                </TabPanelBodyContentComponent>
+                <TabPanelBodyContentComponent id="profile">
+                  <h3>Profile</h3>
+                </TabPanelBodyContentComponent>
+              </TabPanelBodyComponent>
+            </TabPanelComponent>
+          </Panel>
         </div>
-        {/* source: */}
-        <div className="row">
-          <div className="col-xs-12">
-            <Panel
-              title="Source"
-              hasTitle={true}>
-              <Highlight className="javascript">
-                {source}
-              </Highlight>
-            </Panel>
-          </div>
+      </div>
+      {/* source: */}
+      <div className="row">
+        <div className="col-xs-12">
+          <Panel title="Source" hasTitle>
+            <Highlight className="javascript">{source}</Highlight>
+          </Panel>
         </div>
-      </AnimatedView>
-    );
-  }
+      </div>
+    </AnimatedView>
+  );
 }
 
-TabPanel.propTypes= {
+TabPanel.displayName = 'TabPanel';
+
+TabPanel.propTypes = {
   actions: PropTypes.shape({
     enterTabPanel: PropTypes.func.isRequired,
-    leaveTabPanel: PropTypes.func.isRequired
-  })
+    leaveTabPanel: PropTypes.func.isRequired,
+  }),
 };
 
 export default TabPanel;
