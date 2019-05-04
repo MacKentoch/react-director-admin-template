8d9c89e608be0ae7ecf4d0a3190de3f578ab4ac3
MacKentoch <erwan.datin@gmail.com>
2019-05-04 07:21:46 +0200

    migration

---
+++ src/front/views/twitterFeed/DemoTweetMessage.js
@@ -0,0 +1,20 @@
+// @flow
+
+import React from 'react';
+
+function DemoTweetMessage() {
+  return (
+    <p>
+      In hac
+      <a href="#">habitasse</a>
+      platea dictumst. Proin ac nibh rutrum lectus rhoncus eleifend.
+      <a href="#" className="text-danger">
+        <strong>#dev</strong>
+      </a>
+    </p>
+  );
+}
+
+DemoTweetMessage.displayName = 'DemoTweetMessage';
+
+export default DemoTweetMessage;
--- src/front/views/twitterFeed/TwitterFeed.js
+++ src/front/views/twitterFeed/TwitterFeed.js
@@ -1,136 +1,118 @@
-// @flow weak
+// @flow
 
-import React, {
-  PureComponent
-}                         from 'react';
-import PropTypes          from 'prop-types';
-import Highlight          from 'react-highlight';
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import Highlight from 'react-highlight';
 import {
   AnimatedView,
   Panel,
   Tweet,
   WriteNewTweet,
-  ListTweetsContainer
-}                         from '../../components';
-import JannieIMG          from '../../img/Jannie.png';
-import EmmetIMG           from '../../img/Emmet.png';
+  ListTweetsContainer,
+} from '../../components';
+import JannieIMG from '../../img/Jannie.png';
+import EmmetIMG from '../../img/Emmet.png';
+import DemoTweetMessage from './DemoTweetMessage';
 
-function DemoTweetMessage() {
+type Props = {
+  actions: {
+    enterTwitterFeed: () => {},
+    leaveTwitterFeed: () => {},
+  },
+};
+
+const source = `
+  // import
+  import {
+    Tweet,
+    WriteNewTweet,
+    ListTweetsContainer
+  } from './_SOMEWHERE_/components';
+
+  // import image or use relative path as src to image
+  // in this example, consider:
+  // import JannieIMG          from '../../../img/Jannie.png';
+  // import EmmetIMG           from '../../../img/Emmet.png';
+
+  // in render():
+  <Panel
+    title="Twitter feed"
+    hasTitle={true}
+    bodyBackGndColor={'#FFF'}>
+    <WriteNewTweet />
+    <ListTweetsContainer>
+      <Tweet
+        time={'30 min ago'}
+        author={'Emmet'}
+        authorAvatar={EmmetIMG}>
+        <DemoTweetMessage />
+      </Tweet>
+      <Tweet
+        time={'35 min ago'}
+        author={'Jannie'}
+        authorAvatar={JannieIMG}>
+        <DemoTweetMessage />
+      </Tweet>
+    </ListTweetsContainer>
+  </Panel>
+`;
+
+function TwitterFeed({
+  actions: { enterTwitterFeed, leaveTwitterFeed },
+}: Props) {
+  useEffect(() => {
+    enterTwitterFeed();
+
+    return () => {
+      leaveTwitterFeed();
+    };
+  }, []);
+
   return (
-    <p>
-      In hac
-      <a href="#">
-        habitasse
-      </a>
-       platea dictumst. Proin ac nibh rutrum lectus rhoncus eleifend.
-      <a
-        href="#"
-        className="text-danger">
-        <strong>
-          #dev
-        </strong>
-      </a>
-    </p>
+    <AnimatedView>
+      {/* preview: */}
+      <div className="row">
+        <div className="col-xs-6 col-xs-offset-3">
+          <Panel title="Twitter feed" hasTitle={true} bodyBackGndColor={'#FFF'}>
+            <WriteNewTweet />
+            <ListTweetsContainer>
+              <Tweet
+                time={'30 min ago'}
+                author={'Emmet'}
+                authorAvatar={EmmetIMG}
+              >
+                <DemoTweetMessage />
+              </Tweet>
+              <Tweet
+                time={'35 min ago'}
+                author={'Jannie'}
+                authorAvatar={JannieIMG}
+              >
+                <DemoTweetMessage />
+              </Tweet>
+            </ListTweetsContainer>
+          </Panel>
+        </div>
+      </div>
+      {/* source: */}
+      <div className="row">
+        <div className="col-xs-12">
+          <Panel title="Source" hasTitle={true}>
+            <Highlight className="javascript">{source}</Highlight>
+          </Panel>
+        </div>
+      </div>
+    </AnimatedView>
   );
 }
 
-class TwitterFeed extends PureComponent {
-  static propTypes= {
-    actions: PropTypes.shape({
-      enterTwitterFeed: PropTypes.func.isRequired,
-      leaveTwitterFeed: PropTypes.func.isRequired
-    })
-  };
+TwitterFeed.displayName = 'twitterFeed';
 
-  componentWillMount() {
-    const { actions: { enterTwitterFeed } } = this.props;
-    enterTwitterFeed();
-  }
-
-  componentWillUnmount() {
-    const { actions: { leaveTwitterFeed } } = this.props;
-    leaveTwitterFeed();
-  }
-
-  render() {
-    const source = `
-      // import
-      import {
-        Tweet,
-        WriteNewTweet,
-        ListTweetsContainer
-      } from './_SOMEWHERE_/components';
-
-      // import image or use relative path as src to image
-      // in this example, consider:
-      // import JannieIMG          from '../../../img/Jannie.png';
-      // import EmmetIMG           from '../../../img/Emmet.png';
-
-      // in render():
-      <Panel
-        title="Twitter feed"
-        hasTitle={true}
-        bodyBackGndColor={'#FFF'}>
-        <WriteNewTweet />
-        <ListTweetsContainer>
-          <Tweet
-            time={'30 min ago'}
-            author={'Emmet'}
-            authorAvatar={EmmetIMG}>
-            <DemoTweetMessage />
-          </Tweet>
-          <Tweet
-            time={'35 min ago'}
-            author={'Jannie'}
-            authorAvatar={JannieIMG}>
-            <DemoTweetMessage />
-          </Tweet>
-        </ListTweetsContainer>
-      </Panel>
-      `;
-
-    return(
-      <AnimatedView>
-        {/* preview: */}
-        <div className="row">
-          <div className="col-xs-6 col-xs-offset-3">
-            <Panel
-              title="Twitter feed"
-              hasTitle={true}
-              bodyBackGndColor={'#FFF'}>
-              <WriteNewTweet />
-              <ListTweetsContainer>
-                <Tweet
-                  time={'30 min ago'}
-                  author={'Emmet'}
-                  authorAvatar={EmmetIMG}>
-                  <DemoTweetMessage />
-                </Tweet>
-                <Tweet
-                  time={'35 min ago'}
-                  author={'Jannie'}
-                  authorAvatar={JannieIMG}>
-                  <DemoTweetMessage />
-                </Tweet>
-              </ListTweetsContainer>
-            </Panel>
-          </div>
-        </div>
-        {/* source: */}
-        <div className="row">
-          <div className="col-xs-12">
-            <Panel
-              title="Source"
-              hasTitle={true}>
-              <Highlight className="javascript">
-                {source}
-              </Highlight>
-            </Panel>
-          </div>
-        </div>
-      </AnimatedView>
-    );
-  }
-}
+TwitterFeed.propTypes = {
+  actions: PropTypes.shape({
+    enterTwitterFeed: PropTypes.func.isRequired,
+    leaveTwitterFeed: PropTypes.func.isRequired,
+  }),
+};
 
 export default TwitterFeed;
