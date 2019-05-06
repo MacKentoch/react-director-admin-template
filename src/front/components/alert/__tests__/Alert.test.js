a8bc9b6cdc0aecdf40f2098c6f92d226d0241f0f
MacKentoch <erwan.datin@gmail.com>
2019-05-06 05:19:01 +0200

    migration

---
+++ src/front/components/alert/__tests__/Alert.test.js
@@ -0,0 +1,17 @@
+// @flow
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import Alert from '../Alert';
+
+describe('Alert component', () => {
+  it('renders as expected', () => {
+    const component = shallow(
+      <Alert type="info">
+        <p>an alert</p>
+      </Alert>,
+    );
+
+    expect(component).toMatchSnapshot();
+  });
+});
