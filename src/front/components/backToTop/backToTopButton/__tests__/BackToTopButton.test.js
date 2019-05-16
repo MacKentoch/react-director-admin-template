1fe18f43dcd1e17525e63d099558af6f16c3493f
MacKentoch <erwan.datin@gmail.com>
2019-05-16 05:24:16 +0200

    migration

---
+++ src/front/components/backToTop/backToTopButton/__tests__/BackToTopButton.test.js
@@ -0,0 +1,20 @@
+// @flow
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import BackToTopButton from '../BackToTopButton';
+
+describe('BackToTopButton component', () => {
+  it('renders as expected', () => {
+    const props = {
+      onClick: () => {},
+      position: 'bottom-right',
+    };
+    const component = shallow(
+      <BackToTopButton {...props}>
+        <p>button children</p>
+      </BackToTopButton>,
+    );
+    expect(component).toMatchSnapshot();
+  });
+});
