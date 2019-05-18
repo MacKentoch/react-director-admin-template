e4bd536707df85551ab34cdabe7be8df0aadaa54
MacKentoch <erwan.datin@gmail.com>
2019-05-18 07:25:41 +0200

    migration

---
+++ src/front/components/backToTop/BackToTop.test.js
@@ -0,0 +1,18 @@
+// @flow
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import BackToTop from './BackToTop';
+
+describe('BackToTop component', () => {
+  it('renders as expected', () => {
+    const props = {
+      minScrollY: 10,
+      scrollTo: null,
+      onScrollDone: jest.fn(),
+    };
+
+    const component = shallow(<BackToTop {...props} />);
+    expect(component).toMatchSnapshot();
+  });
+});
