1732745f2ce200de965bb03e01f5634faa5e3b12
MacKentoch <erwan.datin@gmail.com>
2019-06-11 05:25:02 +0200

    migration

---
+++ src/front/components/progressBar/ProgressBar.test.js
@@ -0,0 +1,23 @@
+// @flow
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import ProgressBar from './ProgressBar';
+
+describe('Panel component', () => {
+  it('renders as expected', () => {
+    const props = {
+      barSize: 'progress',
+      barStriped: false,
+      barStyle: 'primary',
+      active: false,
+      valueNow: 0,
+      valueMin: 0,
+      valueMax: 100,
+      screenReadersText: 'progress',
+    };
+
+    const component = shallow(<ProgressBar {...props} />);
+    expect(component).toMatchSnapshot();
+  });
+});
